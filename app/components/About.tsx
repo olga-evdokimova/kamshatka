950b5e444bc79c0f218ff305cfaf22a3f941a9a2
olga-evdokimova <pavedom13@gmail.com>
2024-09-08 12:04:24 +0300

    About

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,18 +1,49 @@
+"use client";
 import Container from "./Container";
 import Title from "./Title";
 import Image from "next/image";
 import { consts, data } from "../consts";
 import PageList from "./PageList";
-
+import SubTitle from "./SubTitle";
+import Link from "next/link";
+import { ArrowRightOutlined } from "@ant-design/icons";
 export default function About() {
-
   return (
     <Container id="about">
       <Title>
         <span className="text-[var(--accent)]">Сергей Рудковский</span> —
         провезу вас по самым красивым местам Камчатки на своем джипе!
       </Title>
-      <div className="grid grid-cols-11 grid-rows-[210px] laptop:block gap-[20px] laptop:gap-0 h-[500px]   mt-[50px] tablet:h-[300px]">
+      <div
+        className="mt-[30px] rounded-[var(--border-radius)] flex justify-between  shadow-3xl 
+     mobile:flex-col"
+      >
+        <div className="py-[10px] flex flex-col w-[60%] mobile:w-full">
+          <p className="">Федерация спортивного туризма россии</p>
+          <SubTitle className="text-[var(--accent)] pb-[8px] mb-auto ">
+            Инструктор-проводник
+          </SubTitle>
+          <Link
+            target="_blank"
+            href="https://knd.gov.ru/license?registryType=ATTIncructorProvodnik&id=6683a3ced2b513840f64a94c"
+            className=""
+          >
+            Регистрационный номер{" "}
+            <span className="text-[var(--accent)] text-[20px] pl-[10px] relative group">
+              2439
+              <ArrowRightOutlined className="[&>svg]:fill-[var(--accent)]  rotate-[-45deg] absolute right-[-30px] top-[4px] group-hover:rotate-[45deg] transition-all duration-500" />
+            </span>
+          </Link>
+        </div>
+        <Image
+          className="py-[10px] pb-[10px] object-cover "
+          src="/qr-code.png"
+          width={130}
+          height={130}
+          alt="qr-code"
+        />
+      </div>
+      <div className="grid grid-cols-11 grid-rows-[210px] laptop:block gap-[20px] laptop:gap-0 h-[500px]   mt-[50px] tablet:mt-[20px] tablet:h-[300px]">
         <div className="relative row-span-5 col-span-3 laptop:hidden h-[500px] ">
           <video
             src="/vachkazhets/vachkazhets-02.mp4"
